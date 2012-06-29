686c949ca373ad85df07244f2c4f063d1abde95a
Austin Yun <w.austin.yun@gmail.com>
2012-06-28 18:03:28 -0700

    Initial main commit (skeleton)

---
+++ rice.js
@@ -0,0 +1,15 @@
+var http = require('http'),
+fs = require('fs'),
+path = require('path'),
+maple = require('mapleTree'),
+router = new maple.RouteTree();
+
+function rice() {
+
+// TODO configure router
+  router.define('/', function (req, res) {});
+
+
+  return http.createServer(function (req, res) {
+// TODO should be router.match() something I think
+}).listen(8080);
