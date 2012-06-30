74209304f7551bebced653f74adee19f29f3c882
Austin Yun <w.austin.yun@gmail.com>
2012-06-30 15:44:24 -0700

    Initial working server

--- rice.js
+++ rice.js
@@ -1,15 +1,32 @@
 var http = require('http'),
+    git = require('git-fs'),
     fs = require('fs'),
     path = require('path'),
     router = require('choreographer').router();
 
 module.exports = function rice() {
 
+  // Initialize git-fs with the cwd
+  git(process.cwd());
+
+  router.get('/favicon*', function(req, res) {
+    console.log("No favicon");
+  })
+
+  router.get('/*', function(req, res) {
+    console.error("route hit");
+    var test = git.readFile("fs", req.url, function(err, data) {
+      if (err) { throw err; }
+      console.log(data.toString());
+      res.end(data.toString());
+    })
+  })
+
   router.notFound( function(req, res) {
     res.writeHead(404, {'Content-Type': 'text/plain'});
     res.end('Error 404: ' + req.url + ' not found.');
 
-  });
+  })
 
   return http.createServer(router);
 }
--- test.js
+++ test.js
@@ -3,15 +3,7 @@ var Git = require('git-fs'),
 
 Git(process.cwd());
 
-// Git.safe(function (version) {
-//   if (err) { throw err; } 
-//   Git.readFile(version, "rice.js", function(err, data) {
-//     if (err) {throw err;}
-//     util.p(data);
-//   })
-// });
-
-Git.readFile("fs", "rice.js", function(err, data) {
+Git.readDir("fs", "entries", function(err, data) {
   if (err) { throw err; }
-  console.log(data.toString());
+  console.log(data.files.toString());
 });
