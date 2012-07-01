33ace5f4075b413ef078e789bcf275bc08626115
Austin Yun <w.austin.yun@gmail.com>
2012-07-01 16:48:09 -0700

    Fix package.json again

--- rice.js
+++ rice.js
@@ -11,9 +11,9 @@ module.exports = function rice() {
 
   router.get("/", function(req, res) {
     git.getHead( function(err, head) {
-      if (err) { throw err; }
+      // if (err) { throw err; }
       git.readFile(head, "templates/index.jade", function(err, data) {
-        if (err) { throw err; }
+        // if (err) { throw err; }
         res.writeHead(200, { "Content-Type": "text/html"});
         res.end(jade.compile(data, {"filename":"templates/index.jade"})(), "utf-8");
       });
@@ -22,7 +22,7 @@ module.exports = function rice() {
 
   router.get("/favicon.ico", function(req, res) {
     fs.readFile("public/images/favicon.ico", function(err, icon) {
-      if (err) { throw err; }
+      // if (err) { throw err; }
       console.log("Favicon requested");
       res.writeHead(200, { "Content-Type": "image/x-icon"});
       res.end(icon);
@@ -31,7 +31,7 @@ module.exports = function rice() {
 
   router.get("/public/**", function(req, res, path) {
     fs.readFile("public/" + path, function(err, data) {
-      if (err) { throw err; }
+      // if (err) { throw err; }
       console.log(path);
       res.end(data);
     });
@@ -39,7 +39,7 @@ module.exports = function rice() {
 
   router.get("/public/stylesheets/**", function(req, res, path) {
     fs.readFile("public/stylesheets/" + path, function(err, data) {
-      if (err) { throw err; }
+      // if (err) { throw err; }
       console.log(path);
       res.end(data);
     });
@@ -47,7 +47,7 @@ module.exports = function rice() {
 
   router.get("/public/images/**", function(req, res, path) {
     fs.readFile("public/images/" + path, function(err, data) {
-      if (err) { throw err; }
+      // if (err) { throw err; }
       console.log(path);
       res.end(data);
     });
