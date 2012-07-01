c87b005c10192e9d9e948ebffd878b0f8a07da50
Austin Yun <w.austin.yun@gmail.com>
2012-07-01 16:31:04 -0700

    Heroku ports

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
-var rice = require('./rice')
+var rice = require('./rice'),
+    port = process.env.PORT || 3000;
 
-rice().listen(80);
+rice().listen(port, function() {
+  console.log("Listening on port: " + port);
+});
