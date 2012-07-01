014b5ec70105a1e2db49d5aa8e456ceb79350dac
Austin Yun <w.austin.yun@gmail.com>
2012-07-01 14:19:39 -0700

    Jade working, kind of

--- rice.js
+++ rice.js
@@ -1,47 +1,73 @@
 var http = require("http"),
     fs = require("fs"),
     git = require("git-fs"),
+    jade = require("jade"),
     router = require("choreographer").router();
 
 module.exports = function rice() {
 
-  // Someone tell me if the function scope here is fine or if I need to wrap
-  // this in a var write404 = function(req, res) {
-
-  function write404(req, res) {
-    res.writeHead(404, { "Content-Type": "text/plain"});
-    res.end("Error 404: " + req.url + " not found.");
-    return res;
-  }
   // Initialize git-fs with the cwd
   git(process.cwd());
 
   router.get("/", function(req, res) {
     git.getHead( function(err, head) {
       if (err) { throw err; }
-      git.readFile(head, "index.html", function(err, data) {
+      git.readFile(head, "skeleton.jade", function(err, data) {
         if (err) { throw err; }
         res.writeHead(200, { "Content-Type": "text/html"});
-        res.end(data, "utf-8");
+        console.log(jade.compile(data)());
+        res.end(jade.compile(data)(), "utf-8");
+        console.log("Served INDEX");
       });
     });
   });
 
   router.get("/favicon.ico", function(req, res) {
-    res.writeHead(200, { "Content-Type": "image/x-icon" });
-    res.end(fs.readFile("favicon.ico", function(err, icon) {
-      if (err) { throw err; }
-      return icon;
-    }));
+    fs.readFile("public/favicon.ico", function(err, icon) {
+      console.log("Favicon requested");
+      res.writeHead(200, { "Content-Type": "image/x-icon"});
+      res.end(icon);
+    })
   });
 
-  router.get("/*", function(req, res) {
-    console.error("Variable route hit");
-    git.readFile("fs", req.url, function(err, data) {
-      if (err) { return write404(req, res); }
-      console.log(data.toString()); // Debugging code
-      res.end(data.toString());
+  router.get("/public/**", function(req, res, path) {
+    fs.readFile("public/" + path, function(err, data) {
+      console.log(path);
+      res.end(data);
     });
+  });
+
+  router.get("/public/stylesheets/**", function(req, res, path) {
+    fs.readFile("public/stylesheets/" + path, function(err, data) {
+      console.log(path);
+      res.end(data);
+    });
+  });
+
+  router.get("/public/images/**", function(req, res, path) {
+    fs.readFile("public/images/" + path, function(err, data) {
+      console.log(path);
+      res.end(data);
+    });
+  });
+
+
+  router.get("/*", function(req, res, path) {
+    console.error("Variable route hit");
+    fs.readFile(path, function(err, data) {
+      console.log("Var serving " + path);
+      res.end(data);
+    })
+    // git.readFile("fs", req.url, function(err, data) {
+    //   console.log(data.toString()); // Debugging code
+    //   res.end(data.toString());
+    // });
+  });
+
+  router.notFound(function(req, res) {
+    res.writeHead(404, { "Content-Type": "text/plain"});
+    res.end("Error 404: " + req.url + " not found.");
+    console.log("router.notFound called on " + req.url);
   });
 
   return http.createServer(router);
