7862c6de03d24f711116617166bab327f797f5e7
Austin Yun <w.austin.yun@gmail.com>
2012-06-30 17:36:39 -0700

    Fixed some horrible formatting errors.
    Double quote all the things

--- rice.js
+++ rice.js
@@ -1,32 +1,43 @@
-var http = require('http'),
-    git = require('git-fs'),
-    fs = require('fs'),
-    path = require('path'),
-    router = require('choreographer').router();
+var http = require("http"),
+    fs = require("fs"),
+    git = require("git-fs"),
+    path = require("path"),
+    router = require("choreographer").router();
 
 module.exports = function rice() {
 
   // Initialize git-fs with the cwd
   git(process.cwd());
 
-  router.get('/favicon*', function(req, res) {
-    console.log("No favicon");
-  })
+  router.get("/", function (req, res) {
+    res.end("Homepage");
+  });
 
-  router.get('/*', function(req, res) {
+  router.get("/favicon.ico", function (req, res) {
+    res.writeHead(200, { "Content-Type": "image/x-icon" });
+    res.end(fs.readFile("favicon.ico", function(err, icon) {
+      if (err) { throw err; }
+      return icon;
+    }));
+  });
+
+  router.get("/*", function (req, res) {
     console.error("route hit");
-    var test = git.readFile("fs", req.url, function(err, data) {
+
+    // At the moment, throws an exception and crashes if you try for a file
+    // that's not there
+
+    git.readFile("fs", req.url, function (err, data) {
       if (err) { throw err; }
       console.log(data.toString());
       res.end(data.toString());
-    })
-  })
+    });
+  });
 
-  router.notFound( function(req, res) {
-    res.writeHead(404, {'Content-Type': 'text/plain'});
-    res.end('Error 404: ' + req.url + ' not found.');
-
-  })
+  router.notFound( function (req, res) {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Error 404: " + req.url + " not found.");
+  });
 
   return http.createServer(router);
-}
+};
