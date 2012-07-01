f814a2c2b40a00e0599c111d0ae49c94c9ad77a3
Austin Yun <w.austin.yun@gmail.com>
2012-06-30 17:39:28 -0700

    Delete extraneous files.

--- test.js
+++
@@ -1,9 +0,0 @@
-var Git = require('git-fs'),
-    http = require('http');
-
-Git(process.cwd());
-
-Git.readDir("fs", "entries", function(err, data) {
-  if (err) { throw err; }
-  console.log(data.files.toString());
-});
