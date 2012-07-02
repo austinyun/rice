92f1ae003a604bfec12430c613a1c4c0fe88c18a
Austin Yun <w.austin.yun@gmail.com>
2012-07-01 18:20:06 -0700

    I like port 8080 better than 3000

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var rice = require('./rice'),
-    port = process.env.PORT || 3000,
+    port = process.env.PORT || 8080,
     home = process.env.HOME;
 
 rice( (home === "/app") ? home + "/" : process.cwd()
