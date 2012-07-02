a3435c96f5dd5b847bbb877b2db11bff173cc549
Austin Yun <w.austin.yun@gmail.com>
2012-07-02 16:34:48 -0700

    Better error handling

--- rice.js
+++ rice.js
@@ -19,11 +19,16 @@ module.exports = function rice() {
       "date":"2012-01-02",
       "summary":"Summary of the article"
     }
-  };
+  },
+      write404 = function(req, res, err) {
+        err && console.log(error);
+        res.writeHead(404, { "Content-Type": "text/plain"});
+        res.end("Error 404: " + req.url + " not found.");
+      };
 
   router.get("/", function(req, res) {
     fs.readFile("templates/index.dot", function(err, template) {
-      if (err) { throw err; }
+      if (err) { return write404(req, res, err); }
       res.writeHead(200, { "Content-Type": "text/html"});
       res.end(dot.template(template)(testArticleList));
     });
@@ -31,30 +36,23 @@ module.exports = function rice() {
 
   router.get("/favicon.ico", function(req, res) {
     fs.readFile("public/images/favicon.ico", function(err, icon) {
-      if (err) { throw err; }
+      if (err) { return write404(req, res, err); }
       console.log("Favicon requested");
       res.writeHead(200, { "Content-Type": "image/x-icon"});
       res.end(icon);
     })
   });
 
-  router.get("/public/**", function(req, res, path) {
-    fs.readFile("public/" + path, function(err, data) {
-      if (err) { throw err; }
-      res.end(data);
+  router.get("/public/stylesheets/*", function(req, res, path) {
+    fs.readFile("public/stylesheets/" + path, function(err, stylesheet) {
+      if (err) { return write404(req, res, err); }
+      res.end(stylesheet);
     });
   });
 
-  router.get("/public/stylesheets/**", function(req, res, path) {
-    fs.readFile("public/stylesheets/" + path, function(err, data) {
-      if (err) { throw err; }
-      res.end(data);
-    });
-  });
-
-  router.get("/public/images/**", function(req, res, path) {
+  router.get("/public/images/*", function(req, res, path) {
     fs.readFile("public/images/" + path, function(err, data) {
-      if (err) { throw err; }
+      if (err) { return write404(req, res, err); }
       res.end(data);
     });
   });
@@ -62,12 +60,7 @@ module.exports = function rice() {
   router.get("/*", function(req, res, path) {
     console.log("Variable route hit");
     fs.readFile("posts/" + path + ".md", function(err, data) {
-      if (err) { 
-        // Here's the 404 path
-        console.log("Variable route did not exist");
-        res.writeHead(404, { "Content-Type": "text/plain"});
-        return res.end("Error 404: " + req.url + " not found.");
-      }
+      if (err) { return write404(req, res, err); }
       console.log("Var serving " + path);
       res.end(data);
     })
